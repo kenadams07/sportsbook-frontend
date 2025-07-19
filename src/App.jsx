66183d7dc180b864f9173743638fb0f5b9abae8d
prakash1125 <prakash.threearrow@gmail.com>
2025-07-19 12:02:53 +0400

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">App</h1>
-    </div>
+    <>
+      <h1 className='text-blue-500 hover:text-emerald-500'>Hello world</h1>
+    </>
   )
 }
 
