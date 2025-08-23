100d741d25007e0e7bc2b2169a1475921a4ee78d
ashishagni9x <ashish@9xtechnology.com>
2025-08-23 17:25:44 +0400

    env changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { AppGateway } from './app.gateway';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST', 'localhost'),
+        host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT', 5432),
         username: configService.get<string>('DB_USERNAME', 'postgres'),
         password: configService.get<string>('DB_PASSWORD', '1478'),
